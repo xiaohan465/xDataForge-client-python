dce4fdd4d9edf3a18c34dd13fdc079f42ab4f8e0
XD <thomaspaine@foxmail.com>
2024-07-02 01:02:27 +0800

    add some comment

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -3,14 +3,17 @@ from xdataforge import XDataForge
 
 def main():
     client = XDataForge(base_url="http://localhost:8081", token="sk_wkOQkZSniwQdtMVAAxTFp9JxOxviEFsP", )
+    # setup client, fetch or create run
     client.setup("Eleanor Test Project", "23434", run_id="RUN-1719836666-0z8f2J")
     print(client.run_id)
+    # fetch tasks
     tasks = client.fetch_tasks()
     print(tasks)
+    # for each task, fetch datapoints and process
     task = tasks[0]
     for datapoint in task.fetch_next_datapoint():
         print(datapoint)
-        #process
+        # process
         task.commit_result("test")
 
 if __name__ == "__main__":
